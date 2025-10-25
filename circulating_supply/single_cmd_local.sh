815e88b7362f097ea99142511b9070e6d0ffa904
howard-xie-dev <howard@share.xyz>
2025-10-24 17:00:26 -0700

    Create single_cmd_local.sh

---
+++ circulating_supply/single_cmd_local.sh
@@ -0,0 +1,59 @@
+curl --location 'http://localhost:8000/action/133aBDtToTraVi6k3wae5KaToK55mE9NVnrHZRnokhoC-solana%3A101-2DyCEgk9LL7VgoADDQ6zPYT7aaGUyYpvcz2neChhPE7jWHZEWH1HtSvMHgRuymwu63PuDXhoRGC1DcrqgQPKJzKb' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/133aBDtToTraVi6k3wae5KaToK55mE9NVnrHZRnokhoC-solana%3A101-3jH75x4t9j4SbiJ151xA9PzWwmJLBPt2zoxS8r7pV2qVX5AEL4RckDrxCcyR94Xd5iedWTQ7PtWVanfync9VbnsB' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/133aBDtToTraVi6k3wae5KaToK55mE9NVnrHZRnokhoC-solana%3A101-4TYrJE35CrzoYsW1a7eZ87nwBSENA7qrMz4SVWVXtWs85Es9sr5FYC4FvvRer7WP7SPW5qPYNvaUWFXhS3NQL3rR' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/133aBDtToTraVi6k3wae5KaToK55mE9NVnrHZRnokhoC-solana%3A101-5M3tHrnRNvgP3UUFYatkfcN4fCFishuU7BoRXwam1DnW1At8ZyhSAj49PPonWTAhAUgR1pu9CKh72tbVsbKv34FA' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/13cR1JDT1Cd9mqmQe6ZhTsv8rtf6zsuk9k9piEdPeBGd-solana%3A101-2fgKnG85Gryk9WVYKcQafBWGXctFHTTQhVpunnqT9w1vK7MEcDJnjKN7RqTCEnwfQAHDM8hRSw2G5QREjWHWoH7k' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/13cR1JDT1Cd9mqmQe6ZhTsv8rtf6zsuk9k9piEdPeBGd-solana%3A101-32bqKt4qsh1c9H2gmpY9daJEpHnPob3mn12PDq9fQBfMYQ5V6oRZqCgCNfycR5HHRmAtAZfUiirdnkM7XKPNJeoo' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/13cR1JDT1Cd9mqmQe6ZhTsv8rtf6zsuk9k9piEdPeBGd-solana%3A101-3DgrxVh95SucNCGbXSdmtK8FrputRpHHgkCbGpN3miRNmwNr4NVkDBrVFXHPDNQg8Lfiti8ABgH7eHZ5vdwmmsYR' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/13cR1JDT1Cd9mqmQe6ZhTsv8rtf6zsuk9k9piEdPeBGd-solana%3A101-4VsKpoFDfhXvTQUepZYGJMyJr63pkUfkPJTigyBtXRZuKskzWA6mhL6Pre6uTqNqKBqNvwGij9AHYZz4Xn4fvVrw' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/13cR1JDT1Cd9mqmQe6ZhTsv8rtf6zsuk9k9piEdPeBGd-solana%3A101-5GttCMpCCCLbjbrZrsX9sjHEsZTqG2rPpKrPpMksnjiU5btK5NXdVakyh1kKpgTkCPTSqKnULWZnuHN4WwmYJgLZ' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/1P8uj9XYKu8Qyup7UNVZ2GbGUenb94nEtuffYhVuQXw-solana%3A101-4aRREXNJ5Uefr1Ybab3aNvYrNGivkx3UNLsAmD2YQRE2NXWAJEs7VYSfKVsyrQFBJ6bT1hCPp9YmcJpn41G2XWFj' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/23myTbpGjvQJgRkED6RMHuDvEbvw7wuXGaehi5PR1kyD-solana%3A101-35Kxwd83TAVN3LdymiRnGVG29eQG9xd4GpGEBW8MWggsskUH5eqPXaLBwfoWdgHdredV1CxGAVhDGdKusSMamHTo' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
+
+curl --location 'http://localhost:8000/action/23myTbpGjvQJgRkED6RMHuDvEbvw7wuXGaehi5PR1kyD-solana%3A101-4q8dn68CoL1iBusFLfKi4bfFNhkpYJoDauJqWfK4bqFZJ2oXt39cVFVhXMNNvuQMHn55qV8R5sr36mmdpsvp9YBW' \
+--header 'x-request-id: 0d9d21fb-e26d-41f5-9842-a5ba221cacc8' \
+--header 'X-API-Key: CVr4aTJ6Yu4DPN7mU3VjTg' \
+| jq '.data[].action_items[].extra_fields.swap_info.tokens_bought[].token_circulating_supply'
