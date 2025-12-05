efb5aac9235d842bcb88a37cb65c3232ab17bf37
howard-xie-dev <howard@share.xyz>
2025-12-05 11:12:06 -0800

    Update local_tests_as.sh

--- explore_token/local_tests_as.sh
+++ explore_token/local_tests_as.sh
@@ -1,3 +1,3 @@
- curl --location 'http://localhost:8000/assets/tokens/trending?min_token_age=5&min_token_age_unit=m&max_token_age=30&max_token_age_unit=d' | jq '.data[].created_date'
+ curl --location 'http://localhost:8000/assets/tokens/trending?min_token_age=5m&max_token_age=30d' | jq '.data[].created_date'
 
- curl --location 'http://localhost:8000/assets/tokens/trending?min_wallet_age=5&min_wallet_age_unit=m&max_wallet_age=100&max_wallet_age_unit=d&limit=2' | jq '.data[].created_date'
+ curl --location 'http://localhost:8000/assets/tokens/trending?min_wallet_age=5h&max_wallet_age=30d&limit=2' | jq '.data[].created_date'
