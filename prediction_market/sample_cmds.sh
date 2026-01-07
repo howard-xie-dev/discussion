a35a0dab1997a16a221c84dc9ebe6071c3de585d
howard-xie-dev <howard@share.xyz>
2026-01-07 17:29:53 +0800

    Update sample_cmds.sh

--- prediction_market/sample_cmds.sh
+++ prediction_market/sample_cmds.sh
@@ -20,3 +20,23 @@ curl --request GET \
   --url https://prediction-markets-api.dflow.net/api/v1/event/KXNFLGAME-26JAN04LACDEN \
   --header "x-api-key: pRUpP2gm6MocIOMN7ctj"
 
+# list all series 
+curl --request GET \
+  --url "https://prediction-markets-api.dflow.net/api/v1/series" \
+  --header "x-api-key: pRUpP2gm6MocIOMN7ctj" \
+| jq -r '.series[] | "\(.ticker)\t\(.title)"'
+
+
+# look for a specific series given its ticker 
+ curl --request GET \
+  --url "https://prediction-markets-api.dflow.net/api/v1/series/SENATEMD" \
+  --header "x-api-key: pRUpP2gm6MocIOMN7ctj"
+
+
+#look for events with nested markets
+ curl -sS \
+  "https://prediction-markets-api.dflow.net/api/v1/event/KXNFLGAME-26JAN04LACDEN?withNestedMarkets=true" \
+  -H "x-api-key: pRUpP2gm6MocIOMN7ctj"
+
+
+
