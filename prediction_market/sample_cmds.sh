ad6012478c5888a126d0942abd2f8572defd9147
howard-xie-dev <howard@share.xyz>
2026-01-07 17:34:02 +0800

    Update sample_cmds.sh

--- prediction_market/sample_cmds.sh
+++ prediction_market/sample_cmds.sh
@@ -38,5 +38,43 @@ curl --request GET \
   "https://prediction-markets-api.dflow.net/api/v1/event/KXNFLGAME-26JAN04LACDEN?withNestedMarkets=true" \
   -H "x-api-key: pRUpP2gm6MocIOMN7ctj"
 
+# entity info aggregation
+curl -sS \
+  "https://prediction-markets-api.dflow.net/api/v1/events?withNestedMarkets=true&limit=2" \
+  -H "x-api-key: pRUpP2gm6MocIOMN7ctj" \
+| jq -r '
+  .events[]
+  | . as $e
+  | ($e.markets // [])[]
+  | [
+      ($e.series_ticker // $e.seriesTicker // ""),
+      ($e.ticker // ""),
+      ($e.title // ""),
+      (.ticker // ""),
+      (.title // ""),
+      (.status // ""),
+      (.volume // .volumeUsd // .volume_usd // "" | tostring),
+      (.open_interest // .openInterest // "" | tostring)
+    ]
+  | @tsv
+' \
+| awk 'BEGIN{print "series_ticker\tevent_ticker\tevent_title\tmarket_ticker\tmarket_title\tmarket_status\tmarket_volume\topen_interest"} {print}' \
+| column -t -s $'\t'
+
+
+curl -sS \
+  "https://prediction-markets-api.dflow.net/api/v1/events?withNestedMarkets=true&limit=2" \
+  -H "x-api-key: pRUpP2gm6MocIOMN7ctj" \
+| jq -r '
+  .events[]
+  | . as $e
+  | ($e.markets // [])[]
+  | [($e.series_ticker // $e.seriesTicker // ""), ($e.ticker // ""), (.ticker // "")]
+  | @tsv
+' \
+| awk 'BEGIN{print "series_ticker\tevent_ticker\tmarket_ticker"} {print}' \
+| column -t -s $'\t'
+
+
 
 
