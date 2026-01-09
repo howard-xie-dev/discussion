faf1036eb9c1671bfe75efedef53988ca47ec3bf
howard-xie-dev <howard@share.xyz>
2026-01-09 20:20:02 +0800

    Update sample_cmds_dome_polymarket.sh

--- prediction_market/sample_cmds_dome_polymarket.sh
+++ prediction_market/sample_cmds_dome_polymarket.sh
@@ -12,3 +12,36 @@ curl -s \
   --header "Accept: application/json"
 
 
+curl -s \
+  --url "https://api.domeapi.io/v1/polymarket/markets?limit=10" \
+  --header "Authorization: Bearer $DOME_API_KEY" \
+| jq -r '.markets[].market_slug'
+
+
+ curl -s \
+  --url "https://api.domeapi.io/v1/polymarket/markets?highest-temperature-in-nyc-on-january-11-41forbelow" \
+  --header "Authorization: Bearer $DOME_API_KEY" \
+| jq '.markets[0]'
+
+
+ curl -s \
+  --url "https://gamma-api.polymarket.com/markets/slug/highest-temperature-in-nyc-on-january-11-41forbelow" \
+  --header "Authorization: Bearer $DOME_API_KEY" \
+| jq '{market_slug: .slug, conditionId: .conditionId, events: (.events | map({id, slug, title}))}'
+
+
+curl -s \
+  --url "https://data-api.polymarket.com/positions?user=0x607f420d584632934fc6e351bf55041446c976a2" \
+  --header "Authorization: Bearer $DOME_API_KEY" \
+| jq .
+
+
+curl -s \
+  --url "https://data-api.polymarket.com/closed-positions?user=0x607f420d584632934fc6e351bf55041446c976a2" \
+  --header "Authorization: Bearer $DOME_API_KEY" \
+| jq .
+
+
+
+
+
